dfd85ae5a0cec48dce08f97bcd688953ca45f46c
Go, Eungyeong <dmsrudqmdl328@gmail.com>
2023-09-20 10:48:45 +0900

    five

--- src/a202112029/Main.java
+++ src/a202112029/Main.java
@@ -2,6 +2,7 @@ package a202112029;
 
 public class Main {
 	public static void main(String[]args) {
+		five.print();
 		Two.print();
 		System.out.println("one 202112029");
 		Three.print();
---
+++ src/a202112029/five.java
@@ -0,0 +1,8 @@
+package a202112029;
+
+public class five {
+	public static void print() {
+		System.out.println("five");
+	}
+
+}
